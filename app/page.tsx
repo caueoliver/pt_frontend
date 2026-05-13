10be076f939dd96e342f1e1a197e08ee89e5ab22
caue-oliver <caue.ooliver@gmail.com>
2026-05-13 15:22:38 -0300

    remove: removendo código da página de cadastro na página de feed

--- app/page.tsx
+++ app/page.tsx
@@ -6,52 +6,8 @@ import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { useRouter } from 'next/navigation';
 import { register } from '@/api/api.js';
 
-// formato do fomulario (tudo string)
-type CadastroForm = {
-  nomeCompleto: string;
-  username: string;
-  email: string;
-  senha: string;
-  confirmarSenha: string;
-};
 
-// o Partial fala que nem todos os campos precisam existir ao mesmo tempo. serve pra parte de erros, pq tem campo que tem erro e tem campo que não tem
-type Erros = Partial<CadastroForm>;
-
-export default function TelaCadastro() {
-
-  // faz o redirecionamento
-  const router = useRouter();
-
-  // guarda os valores de cada campo e começa vazio
-  const [form, setForm] = useState<CadastroForm>({
-    nomeCompleto: "",
-    username: "",
-    email: "",
-    senha: "",
-    confirmarSenha: "",
-  });
-
-  // guarda as mensagens de erro de cada campo e começa vazio tbm 
-  const [erros, setErros] = useState<Erros>({});
-
-  // ver se o usuario clicou no olho da senha
-  const [mostrarSenha, setMostrarSenha] = useState(false);
-
-  // mesma coisa mas pro campo de confirmar senha
-  const [mostrarConfirmarSenha, setMostrarConfirmarSenha] = useState(false);
-
-  // essa funcao roda toda vez que o usuario digita em qualquer campo
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target; // pego o nome e o valor do input que foi digitado
-
-    setForm((prev) => ({ ...prev, [name]: value })); // atualiza só o campo que foi digitado
-    setErros((prev) => ({ ...prev, [name]: "" })); // limpa o erro desse campo quando digitado alguma coisa nele
-  };
-
-  // essa funcao ve se tudo foi preenchido certo. ai se tiver tudo ok retorna true se não retorna false
-  
-
+export default function TelaFeed() {
     return (
       <div className="relative min-h-screen bg-[#F6F3E4]">
 
