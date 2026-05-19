1c8bc20a581b82f49cc2638a2289db6832147c51
caue-oliver <caue.ooliver@gmail.com>
2026-05-19 13:03:09 -0300

    fix: posicionamento de imagem e texto

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function TelaFeed() {
       <div className="relative min-h-screen bg-[#F6F3E4]">  
           
         {/* div  para criar o bloco superior*/}
-          <div className="fixed left-[0] right-[0] top-[0] bottom-[50%] bg-[#171918]  items-center pl-[5%] pr-[5%] pt-[3%] pb-[3%] shadow-2xl  ">
+          <div className=" flex items-end fixed left-[0] right-[0] top-[0] bottom-[30%] bg-[#171918]  items-center pl-[5%] pr-[5%] pt-[3%] pb-[] shadow-2xl  ">
            <div>
             {/* redirecionamento temporario antes da implementação da nav bar */}
             <p className="text-left text-white text-lg font-bold pt-5">
@@ -29,23 +29,23 @@ export default function TelaFeed() {
             </p>
            </div>
            
-           
-          <div className='flex justify-between overflow-hidden '>
+          {/*configurando uma div para ter um espaço entre o texto e a imagem*/}
+          <div className='flex justify-between overflow-hidden w-full h-[600px]'>
 
             <div>
-              <h2 className="text-5xl font-black">
+              <h2 className="text-6xl font-black text-right fixed top-[25%]">
               Do CAOS à organização, <br />
               em alguns cliques
               </h2>
             </div>
           
-            <div>
+            <div className='h-full flex items-end pr-[10%] '>
               <img
               src="/img_feed/pessoa_feed.png"
               alt="pessoa da stock.io"
-              width={497}
-              height={1129}
-              />
+
+        
+              className="h-full"/>
             </div>
           
           </div>
