ac0ed099de49a904d4de93c9b80d18ea60aa01ee
caue-oliver <caue.ooliver@gmail.com>
2026-05-25 17:01:27 -0300

    fix: ajustando posição dos elementos na página

--- app/(system)/page.tsx
+++ app/(system)/page.tsx
@@ -9,23 +9,23 @@ export default function TelaFeed() {
       <div className="relative min-h-screen bg-[#F6F3E4]">  
           
         {/* div  para criar o bloco superior*/}
-          <div className=" flex items-end fixed left-[0] right-[0] top-[0] bottom-[30%] bg-[#000000]  items-center pl-[5%] pr-[5%] pt-[3%]  ">
+        <div className="flex items-end fixed left-[0] right-[0] top-[95] bg-[#000000] h-[450] items-center">
            
           {/*configurando uma div para ter um espaço entre o texto e a imagem*/}
-          <div className='w-full flex justify-between overflow-hidden w-full h-[600px]'>
+          <div className='relative w-full h-full flex justify-between overflow-hidden'>
 
-            <div>
+            <div className='pl-[10%] w-full h-full'>
               <h2 className="text-6xl font-black text-right fixed top-[25%]">
               Do CAOS à organização, <br />
-              em alguns cliques
+              em alguns cliques 
               </h2>
             </div>
           
-            <div className='h-full flex items-end pr-[10%] '>
+            <div className='w-full flex items-end pr-[10%]'>
               <img
               src="/img_feed/pessoa_feed.png"
-              alt="logo stock.io"
-              className="h-full"/>
+              alt="pessoa stock.io"
+              className="h-full object-cover"/>
             </div>
           
           </div>
@@ -35,7 +35,7 @@ export default function TelaFeed() {
           
             
 
-          </div>
+        </div>
 
           
       </div>
