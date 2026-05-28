6977c75e4a020580a9ba89fc0d60cecccf109858
caue-oliver <caue.ooliver@gmail.com>
2026-05-27 22:04:10 -0300

    feat: card produto

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
     >
       
       <body className="min-h-full flex flex-col">
-        <NavBar />
           <main>
             {children}
           </main>
